9c49372a2fc984221aaec4ac7e90feeeea654d2d
Eugene Eremin <eugenevc@gmail.com>
2022-04-28 01:16:09 +0300

    Добавлено задание на порождающие паттерны

---
+++ creational_pattern/tests.py
@@ -0,0 +1,26 @@
+import unittest
+from implementation import LiveGame, PoolCell
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_cell_request_100(self):
+        pool = PoolCell()
+        game = LiveGame(pool)
+        for i in range(0, 100):
+            game.give_birth_cell()
+
+        self.assertEqual(pool.size(), 0)
+
+    def test_cell_request_and_free(self):
+        pool = PoolCell()
+        game = LiveGame(pool)
+        for i in range(0, 200):
+            cell = game.give_birth_cell()
+            game.kill_cell(cell)
+
+        self.assertEqual(pool.size(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
